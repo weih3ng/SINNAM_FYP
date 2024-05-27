21020852c457547b60da798f64f995f0c4d1d7c7
sokhiennn <22013179@myrp.edu.sg>
2024-05-27 09:49:07 +0800

    Created signUp.php Added 2.png

---
+++ signUp.php
@@ -0,0 +1,251 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">  <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"> <!-- Font Awesome --> 
+    <title>Registration Page</title>
+    <style>
+        body, html {
+        margin: 0;
+        padding: 0;
+        font-family: Arial, sans-serif;
+        }
+        .navbar, footer {
+            background-color: #80352F;
+            color: white;
+            padding: 10px 20px;
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+        }
+
+        .logo {
+            width: 90px;
+            margin-left: 10px;
+        }
+        .navbar-links a, .footer-links a {
+            color: white;
+            text-decoration: none;
+            margin: 0 25px;
+        }
+
+        .navbar-links {
+            display: flex;
+            flex-grow: 1;  /* This helps to distribute space more evenly */
+            justify-content: center; /* Center all navbar links */
+            gap: 20px; /* Adjust the gap to manage space between navigation links */
+            margin-right: 480px; 
+        }
+        .nav-custom {
+            color: white;
+            text-decoration: none;
+            display: flex;
+            align-items: center;
+            gap: 8px;
+            padding: 5px 10px; /* Small padding around text and icon */
+            margin-left: 10px; /* Manage left margin to bring items closer */
+            margin-right: 10px; /* Manage right margin to bring items closer */
+        }
+
+        .nav-custom:last-child {
+            margin-right: 0; 
+        }
+
+        .container {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            background-color: #F1EDE2;
+            height: calc(150vh - 120px);
+        }
+
+        .left-decoration img {
+            max-width: 730px;
+            margin-left: -120px;
+            margin-top: 35px;
+        }
+
+        .register-form-container {
+            background-color: white;
+            border-radius: 15px;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+            padding: 30px 40px;
+            width: 400px;
+            margin-left: 50px;
+        }
+
+        .register-form-container h1 {
+            text-align: center;
+            margin-bottom: 20px;
+            color: #80352F;
+        }
+
+        .register-form-container p {
+            text-align: center;
+        }
+
+        .register-form-container label {
+            display: block;
+            margin-bottom: 5px;
+        }
+
+        .register-form-container input[type="text"],
+        .register-form-container input[type="email"],
+        .register-form-container input[type="password"],
+        .register-form-container input[type="date"] {
+            width: calc(100% - 22px);
+            padding: 10px;
+            margin-bottom: 15px;
+            border: 1px solid #DC3545;
+            border-radius: 20px;
+            background-color: #F8D7DA;
+
+        }
+
+        .register-form-container input[type="radio"] {
+            margin-left: 10px;
+        }
+
+        .register-form-container .radio-label {
+            margin-right: 15px;
+            
+        }
+
+        .register-form-container .gender-container {
+            display: flex;
+            align-items: center;
+            margin-bottom: 15px;
+            justify-content: space-between;
+        }
+
+        .gender-radio-buttons {
+            display: flex;
+            justify-content: flex-end;
+            flex-grow: 1;
+        }
+
+        .register-form-container .btn {
+            display: block;
+            width: 100%;
+            width: 150px;
+            padding: 10px;
+            background-color: #80352F;
+            color: white;
+            border: none;
+            border-radius: 20px;
+            font-size: 16px;
+            cursor: pointer;
+            margin-top: 10px;
+            margin: 0 auto;
+        }
+
+        .register-form-container .btn:hover {
+            background-color: #6b2c27;
+        }
+
+        .register-form-container .terms {
+            font-size: 15px;
+            text-align: center;
+            margin-top: 10px;
+        }
+        
+        .social-media {
+            margin-top: 2px;
+            
+        }
+
+        .social-media a {
+            color: white;
+            text-decoration: none;
+            font-size: 24px;
+        }
+    </style>
+</head>
+<body>
+        <!-- Navigation Bar -->
+        <div class="navbar">
+            <a class="navbar-brand text-dark" href="home.html">
+                <img src="images/logo.jpeg" alt="logo" class="logo">
+            </a>
+            <div class="navbar-links">
+                <a href="home.html">Home</a>
+                <a href="#">About Us</a>
+                <a href="#">Appointment</a>
+                <a href="#">Contact Us</a>
+            </div>
+    
+            <!-- Sign Up & Login Button -->
+            <a class="nav-custom" href="signup.html">
+                <i class="fa-solid fa-user" style="color: #ffffff;"></i> Sign Up
+            </a>
+    
+            <a class="nav-custom" href="login.php" style="color: #F8D7DA;">
+                <i class="fa-solid fa-right-to-bracket" style="color: #F8D7DA;"></i> Login
+            </a>  
+        </div>
+
+        <!-- Register Container -->
+        <div class="container">
+        <div class="left-decoration">
+            <img src="images/2.png" alt="Decoration">
+        </div>
+        <div class="register-form-container">
+            <h1>Create New Account</h1>
+            <p>Already a member? <a href="login.php">Log in</a></p>
+            <form>
+                <label for="idName">
+                    <i class="fa-solid fa-user" style="color: #949494;"></i> Name
+                </label>
+                <input id="idName" type="text">
+                <label for="idAge">
+                    <i class="fa-solid fa-calendar" style="color: #949494;"></i> Age
+                </label>
+                <input id="idAge" type="text">
+                <label for="idEmail">
+                    <i class="fa-solid fa-envelope" style="color: #949494;"></i> Email
+                </label>
+                <input id="idEmail" type="email">
+                <label for="idPassword">
+                    <i class="fa-solid fa-lock" style="color: #949494;"></i> Password
+                </label>
+                <input id="idPassword" type="password">
+                <label for="idDob">
+                    <i class="fa-solid fa-calendar-alt" style="color: #949494;"></i> Date of Birth
+                </label>
+                <input id="idDob" type="date">
+                <div class="gender-container">
+                    <label class="gender-label">
+                        <i class="fa-solid fa-venus-mars" style="color: #949494;"></i> Gender
+                    </label>
+                    <div class="gender-radio-buttons">
+                    <label class="radio-label">
+                        <input type="radio" name="gender" value="male"> Male
+                    </label>
+                    <label class="radio-label">
+                        <input type="radio" name="gender" value="female"> Female
+                    </label>
+                </div>
+            </div>
+                <button type="submit" class="btn">Sign up</button>
+                <p class="terms">
+                    By clicking "SIGN UP", I acknowledge that I have read, understood and agree that I am bound by the <a href="#">Account Terms of Use</a>
+                </p>
+            </form>
+        </div>
+        </div>
+        
+        <!-- Footer -->
+        <footer>
+            <img src="images/logo.jpeg" alt="logo" class="logo">
+            <div>
+                @ 2024 Sin Nam Medical Hall All Rights Reserved
+            </div>
+            <div class="social-media">
+                <span style="margin-right: 10px;">Follow us</span> <!-- Added a span to apply margin -->
+                <a href="https://www.facebook.com/profile.php?id=167794019905102&_rdr"><i class="fa-brands fa-facebook" style="color: #ffffff;"></i></a>
+            </div>
+        </footer>
+</body>
+</html>-
