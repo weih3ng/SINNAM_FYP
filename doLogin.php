b74a2e7d0a1052d6611b99c04f0e8352fd93fdee
22005564 WeiHeng <132324176+weih3ng@users.noreply.github.com>
2024-05-28 19:00:04 +0800

    D

--- doLogin.php
+++ doLogin.php
@@ -19,32 +19,40 @@ $msg = "";
 $query = "SELECT * FROM patients WHERE email = '$email' AND Password = '$password'";
 $result = mysqli_query($link, $query);
 
-mysqli_close($link); // Closing db connection
-?>
-
-<?php 
 // If results exist, fetch into $row
 if ($result && mysqli_num_rows($result) > 0) {
-
     $row = mysqli_fetch_array($result);
-    $_SESSION['patient_id'] = $row['PatientID'];
-    $_SESSION['username'] = $row['Username'];
-    $_SESSION['name'] = $row['Name'];
-    $_SESSION['birthdate'] = $row['DOB'];
-
     $msg = "Login Successful";
+    $_SESSION['success_message'] = "Successfully logged in.";
 } 
-
 // If no results found, set error msg and redirect to login.php
 else {
     $_SESSION['error_message'] = "Invalid email or password. Please try again.";
     header('Location: login.php');
     $msg = "Login Failed";
-
 }
+
+mysqli_close($link); 
 ?>
 
-
-
-
-
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login Status</title>
+    <style>
+       
+        .center {
+            text-align: center;
+            margin-top: 50px; 
+        }
+        .big-text {
+            font-size: 36px; 
+        }
+    </style>
+</head>
+<body>
+    <div class="center">
+        <p class="big-text"><?php echo isset($_SESSION['success_message']) ? $_SESSION['success_message'] : ''; ?></p>
+    </div>
+</body>
+</html>
