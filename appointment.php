9d7b0e72c41243d0e491f9580c80055921dab777
jocelyntan5 <131931552+jocelyntan5@users.noreply.github.com>
2024-07-13 15:39:01 +0800

    appointment.php once the time has pass, the time slot cannot be booked on the day - can disable the time

--- appointment.php
+++ appointment.php
@@ -419,6 +419,7 @@ mysqli_close($link);
                     timeslotOptions = ["10:30 AM", "10:45 AM", "11:00 AM", "11:15 AM", "11:30 AM", "11:45 AM", "12:00 PM", "12:15 PM", "12:30 PM", "12:45 PM", "1:00 PM", "1:15 PM", "1:30 PM", "1:45 PM", "2:00 PM", "2:15 PM", "2:30 PM", "2:45 PM", "3:00 PM", "3:15 PM", "3:30 PM", "3:45 PM", "4:00 PM", "4:15 PM"];
                 }
 
+                // Filter out timeslots that have passed if the selected date is today (joc)
                 if (isToday) {
                     var currentHours = currentDate.getHours();
                     var currentMinutes = currentDate.getMinutes();
