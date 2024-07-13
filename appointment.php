722ad915258a0c3b9e6a376d6db154df06a9cfd9
22005564 WeiHeng <132324176+weih3ng@users.noreply.github.com>
2024-07-13 18:03:34 +0800

    fix 24hr clock issue

--- appointment.php
+++ appointment.php
@@ -273,20 +273,20 @@ mysqli_close($link);
                         <option value="12:15 PM">12:15 PM</option>
                         <option value="12:30 PM">12:30 PM</option>
                         <option value="12:45 PM">12:45 PM</option>
-                        <option value="1:00 PM">1:00 PM</option>
-                        <option value="1:15 PM">1:15 PM</option>
-                        <option value="1:30 PM">1:30 PM</option>
-                        <option value="1:45 PM">1:45 PM</option>
-                        <option value="2:00 PM">2:00 PM</option>
-                        <option value="2:15 PM">2:15 PM</option>
-                        <option value="2:30 PM">2:30 PM</option>
-                        <option value="2:45 PM">2:45 PM</option>
-                        <option value="3:00 PM">3:00 PM</option>
-                        <option value="3:15 PM">3:15 PM</option>
-                        <option value="3:30 PM">3:30 PM</option>
-                        <option value="3:45 PM">3:45 PM</option>
-                        <option value="4:00 PM">4:00 PM</option>
-                        <option value="4:15 PM">4:15 PM</option>
+                        <option value="1:00 PM">13:00 PM</option>
+                        <option value="1:15 PM">13:15 PM</option>
+                        <option value="1:30 PM">13:30 PM</option>
+                        <option value="1:45 PM">13:45 PM</option>
+                        <option value="2:00 PM">14:00 PM</option>
+                        <option value="2:15 PM">14:15 PM</option>
+                        <option value="2:30 PM">14:30 PM</option>
+                        <option value="2:45 PM">14:45 PM</option>
+                        <option value="3:00 PM">15:00 PM</option>
+                        <option value="3:15 PM">15:15 PM</option>
+                        <option value="3:30 PM">15:30 PM</option>
+                        <option value="3:45 PM">15:45 PM</option>
+                        <option value="4:00 PM">16:00 PM</option>
+                        <option value="4:15 PM">16:15 PM</option>
                     </select><span class="ipsFieldRow_required" style="margin-left: 10px;">Required</span>
 
                     <br><br><br> <!-- Add booking for myself/family member (joc) -->
@@ -414,9 +414,9 @@ mysqli_close($link);
 
                 var timeslotOptions = [];
                 if (dayOfWeek >= 2 && dayOfWeek <= 5) { // Tuesday to Friday
-                    timeslotOptions = ["11:00 AM", "11:15 AM", "11:30 AM", "11:45 AM", "12:00 PM", "12:15 PM", "12:30 PM", "12:45 PM", "1:00 PM", "1:15 PM", "1:30 PM", "1:45 PM", "2:00 PM", "2:15 PM", "2:30 PM", "2:45 PM", "3:00 PM", "3:15 PM", "3:30 PM", "3:45 PM", "4:00 PM", "4:15 PM"];
+                    timeslotOptions = ["11:00 AM", "11:15 AM", "11:30 AM", "11:45 AM", "12:00 PM", "12:15 PM", "12:30 PM", "12:45 PM", "13:00 PM", "13:15 PM", "13:30 PM", "13:45 PM", "14:00 PM", "14:15 PM", "14:30 PM", "14:45 PM", "15:00 PM", "15:15 PM", "15:30 PM", "15:45 PM", "16:00 PM", "16:15 PM"];
                 } else if (dayOfWeek === 6) { // Saturday
-                    timeslotOptions = ["10:30 AM", "10:45 AM", "11:00 AM", "11:15 AM", "11:30 AM", "11:45 AM", "12:00 PM", "12:15 PM", "12:30 PM", "12:45 PM", "1:00 PM", "1:15 PM", "1:30 PM", "1:45 PM", "2:00 PM", "2:15 PM", "2:30 PM", "2:45 PM", "3:00 PM", "3:15 PM", "3:30 PM", "3:45 PM", "4:00 PM", "4:15 PM"];
+                    timeslotOptions = ["10:30 AM", "10:45 AM", "11:00 AM", "11:15 AM", "11:30 AM", "11:45 AM", "12:00 PM", "12:15 PM", "12:30 PM", "12:45 PM", "13:00 PM", "13:15 PM", "13:30 PM", "13:45 PM", "14:00 PM", "14:15 PM", "14:30 PM", "14:45 PM", "15:00 PM", "15:15 PM", "15:30 PM", "15:45 PM", "16:00 PM", "16:15 PM"];
                 }
 
                 // Filter out timeslots that have passed if the selected date is today (joc)
